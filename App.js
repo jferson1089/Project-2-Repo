dd434e89f8241da0f6b964832ebfc64c8edfddbb
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-28 16:54:07 -0400

    files added

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home'
+
+
+function App() {
+  return (
+    <div className="App">
+      Main App loading here...
+      <Home />
+    </div>
+  );
+}
+
+export default App;

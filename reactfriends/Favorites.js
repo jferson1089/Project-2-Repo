6ff2554cca025e5caf017174925da8f1c7dc2565
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-30 12:16:56 -0400

    style updates

---
+++ reactfriends/Favorites.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Favorites() {
+
+    // const handleClick = e => {
+    //     e.stopPropagation();
+    //     console.log("Handling Fave click!");
+    //     props.onFaveToggle();
+    //   };
+
+
+
+
+  return (<div className='fave'>
+      <img className='faveImag' src='' alt='' />
+      <h3>Favorited User Name</h3>
+      <h3>Favorited User Location</h3>
+      <hr/>
+      
+
+
+
+  </div>
+  )
+}
+
+export default Favorites;

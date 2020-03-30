fa179e809a046b18a6bfca6644bc510cdb7f39b0
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-30 12:15:31 -0400

    style update

---
+++ App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
+import Favorites from './components/Favorites'
+import {Route, Link} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+
+    <header>
+      <nav>
+        <ul>
+          <Link to="/">
+            
+              <a href=""><img src='https://res.cloudinary.com/jferson1089/image/upload/c_scale,h_59/v1585540434/iconfinder_React.js_logo_1174949_jjptab.png' alt='logo'>
+                </img>ReactFriends   </a>
+            
+          </Link>
+       
+          </ul>
+             </nav>
+          </header>
+          <main>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about/" component={About} />
+              <Route exact path="/users/" component={Users} />
+              <Route exact path="/users/fave" component={Favorites} />
+
+          </main>
+
+
+<footer>
+  <nav>
+    <ul>
+          <Link to="/users/">
+            <li>
+              <a href="">Users</a>
+            </li>
+          </Link>
+          <Link to="/users/fave">
+            <li>
+              <a href="">Favorites</a>
+            </li>
+          </Link>
+   <Link to="/about/">
+            <li>
+              <a href="">About</a>
+            </li>
+          </Link>
+       
+
+
+
+        </ul>
+      </nav>
+     </footer>
+    </div>
+
+   
+  </>
+            
+   
+    
+  );
+}
+
+export default App;

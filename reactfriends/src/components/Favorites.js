1c5f1a8a9251671fa5386585a175db0f915f0145
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-31 22:32:28 -0400

    favorites rendering in users?

--- reactfriends/src/components/Favorites.js
+++ reactfriends/src/components/Favorites.js
@@ -29,19 +29,27 @@ function Favorites(props) {
     //     )
     // })
   
+    const favoriteReturn = props.fave.map((favorite, index) => {
+        console.log('favorites return?', favorite)
+        return  <li className="favorites" key={index}>
+            <img src={favorite.picture.large} alt='image' />
+            <h6>Favorited User</h6>
+            <h2>{favorite.name.first}</h2>
+            <h3>City:{favorite.location.city}</h3>
 
+        </li>
+    })
     
 
 
   return (<div className='fave' 
-
-  >
+> 
+     {favoriteReturn}
      
-      <p>Favorites?</p>
 
-        {props.fave}
+       
     
-      <hr/>
+     
       
   </div>
   )
